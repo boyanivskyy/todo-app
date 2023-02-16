ce50864501673b2eeadfd7f6643231cbddc83f09
Vitaliy Boyanivskyy <vitaliy.boyanivsky@scalors.com>
2023-02-16 20:59:56 +0200

    init services and repositories

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,16 @@ import (
 
 	"github.com/boyanivskyy/todo-app"
 	"github.com/boyanivskyy/todo-app/pkg/handler"
+	"github.com/boyanivskyy/todo-app/pkg/repository"
+	"github.com/boyanivskyy/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-	server := new(todo.Server)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
+	server := new(todo.Server)
 	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured when running http server: %s", err.Error())
 	}
