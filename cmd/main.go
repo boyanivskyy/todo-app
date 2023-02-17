8f36d54d57bebb66c03195a1c236a8b9953cb3da
Vitaliy Boyanivskyy <vitaliy.boyanivsky@scalors.com>
2023-02-17 09:30:29 +0200

    create db migrations and configs

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,27 @@ import (
 	"github.com/boyanivskyy/todo-app/pkg/handler"
 	"github.com/boyanivskyy/todo-app/pkg/repository"
 	"github.com/boyanivskyy/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err)
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	server := new(todo.Server)
-	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured when running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
