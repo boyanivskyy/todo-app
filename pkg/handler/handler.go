f6e175e62cf2245a32e1d31e3c014c32dd069f00
Vitaliy Boyanivskyy <vitaliy.boyanivsky@scalors.com>
2023-02-20 20:31:05 +0200

    finish with the api

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -31,16 +31,21 @@ func (h *Handler) InitRoutes() *gin.Engine {
 			lists.GET("/:id", h.getListById)
 			lists.PUT("/:id", h.updateList)
 			lists.DELETE("/:id", h.deleteList)
+
+			items := api.Group(":id/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+			}
 		}
 
-		items := api.Group(":id/items")
+		items := api.Group("items")
 		{
-			items.POST("/", h.createItem)
-			items.GET("/", h.getAllItems)
-			items.GET("/:item_id", h.getItemById)
-			items.PUT("/:item_id", h.updateItem)
-			items.DELETE("/:item_id", h.deleteItem)
+			items.GET("/:id", h.getItemById)
+			items.PUT("/:id", h.updateItem)
+			items.DELETE("/:id", h.deleteItem)
 		}
+
 	}
 
 	return router
